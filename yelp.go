1e0b150f141478ee41e707bb4eea6ab3bfd5abf5
biii <ben.share@gmail.com>
2016-12-24 16:21:27 +0800

    support yelp

--- yelp.go
+++ yelp.go
@@ -61,13 +61,16 @@ func yelp_parse(bot *linebot.Client, token string, loc *linebot.LocationMessage,
 			GeneralOptions: &yelp.GeneralOptions{
 				Term: food,
 			},
+			LocationOptions: &yelp.LocationOptions {
+				Location: loc.Address
+			}
 			//LocaleOptions: &yelp.LocaleOptions{
 			//	lang: "zh",
 			//},
-			CoordinateOptions: &yelp.CoordinateOptions{
-				Latitude:  null.FloatFrom(loc.Latitude),
-				Longitude: null.FloatFrom(loc.Longitude),
-			},
+			//CoordinateOptions: &yelp.CoordinateOptions{
+			//	Latitude:  null.FloatFrom(loc.Latitude),
+			//	Longitude: null.FloatFrom(loc.Longitude),
+			//},
 		}
 
 		// Perform the search using the search options
